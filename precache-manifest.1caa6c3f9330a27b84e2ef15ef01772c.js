42115a0d65a06ae4ea580a6fd7686ca9bce94a67
raywu <wu.haoshiang@gmail.com>
2019-10-02 23:01:02 +0800

    Updates

---
+++ precache-manifest.1caa6c3f9330a27b84e2ef15ef01772c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e38e815bf4676e55d3bdbce55bd1df",
+    "url": "/machine-learning-dashboard/index.html"
+  },
+  {
+    "revision": "ac9ca6417d8e71d1df4e",
+    "url": "/machine-learning-dashboard/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3809644a7fd4c4a69c06",
+    "url": "/machine-learning-dashboard/static/js/2.5aafcd63.chunk.js"
+  },
+  {
+    "revision": "ac9ca6417d8e71d1df4e",
+    "url": "/machine-learning-dashboard/static/js/main.3e0c05d0.chunk.js"
+  },
+  {
+    "revision": "7b52e3835a808e424376",
+    "url": "/machine-learning-dashboard/static/js/runtime-main.3d3679b9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/machine-learning-dashboard/static/media/logo.25bf045c.svg"
+  }
+]);
